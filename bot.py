80e3dd505335c92c364b8a2043b790fe1aa9e1d0
anonimous0 <zagin.samovbuvc@gmail.com>
2021-02-16 22:40:33 +0200

    'new'

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot('1378942416:AAGJvCM3qqaQWf0P3HKSQgooQOaOIrc5oNw')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+    @bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+botзpolling(none_stop=True)
